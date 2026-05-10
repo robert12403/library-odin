89506c1d8c2aa37b1d3f5888924f609accd92c04
robert12403 <robert.chiriac1998@icloud.com>
2026-05-10 18:38:00 +0300

    Can add books now

--- javascript.js
+++ javascript.js
@@ -15,20 +15,21 @@ function addBookToLibrary(book) {
 function displayBooks() {
     const library = document.querySelector("#library")
     myLibrary.forEach((book) => {
-        let newBook = document.createElement("div")
+        const newBook = document.createElement("div")
         newBook.classList.add("book")
         library.appendChild(newBook)
 
-        let title = document.createElement("div")
+        const title = document.createElement("div")
         title.textContent = book.title
+        title.setAttribute("style", "font-weight: 900")
 
-        let author = document.createElement("div")
+        const author = document.createElement("div")
         author.textContent = book.author
 
-        let pages = document.createElement("div")
+        const pages = document.createElement("div")
         pages.textContent = book.pages + " pages"
 
-        let read = document.createElement("div")
+        const read = document.createElement("div")
         read.textContent = `Book was read: ${book.read ? 'Yes' : 'No'}`
 
         newBook.appendChild(title)
@@ -38,10 +39,32 @@ function displayBooks() {
     })
 }
 
+function clearDisplay() {
+    const library = document.querySelector("#library")
+    library.innerHTML = ''
+}
+
+const form = document.querySelector("#myForm")
+form.addEventListener('submit', () => {
+    event.preventDefault()
+
+    const title = document.querySelector("#title").value
+    const author = document.querySelector("#author").value
+    const pages = Number(document.querySelector("#pages").value)
+    const read = document.querySelector("#read").value === "Yes"
+
+    const book = new Book(title, author, pages, read, crypto.randomUUID())
+    addBookToLibrary(book)
+
+    form.reset()
+    clearDisplay()
+    displayBooks()
+})
+
 const book1 = new Book("L'Etranger", "Albert Camus", 100, true, crypto.randomUUID())
 const book2 = new Book("Crime and punishment", "Dostoevsky", 300, true, crypto.randomUUID())
 const book3 = new Book("Brothers Karamazov", "Dostoevsky", 900, true, crypto.randomUUID())
-const book4 = new Book("The trial", "Kafka", 200, true, crypto.randomUUID())
+const book4 = new Book("The Trial", "Kafka", 200, true, crypto.randomUUID())
 const book5 = new Book("Les Miserables", "Victor Hugo", 900, false, crypto.randomUUID())
 const book6 = new Book("Candide", "Voltaire", 300, false, crypto.randomUUID())
 
