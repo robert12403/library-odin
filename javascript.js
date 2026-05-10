ce057b14f0c2fe9c82089c4d6deb59417754b21a
robert12403 <robert.chiriac1998@icloud.com>
2026-05-10 17:32:53 +0300

    Can display books

---
+++ javascript.js
@@ -0,0 +1,55 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
+function displayBooks() {
+    const library = document.querySelector("#library")
+    myLibrary.forEach((book) => {
+        let newBook = document.createElement("div")
+        newBook.classList.add("book")
+        library.appendChild(newBook)
+
+        let title = document.createElement("div")
+        title.textContent = book.title
+
+        let author = document.createElement("div")
+        author.textContent = book.author
+
+        let pages = document.createElement("div")
+        pages.textContent = book.pages + " pages"
+
+        let read = document.createElement("div")
+        read.textContent = `Book was read: ${book.read ? 'Yes' : 'No'}`
+
+        newBook.appendChild(title)
+        newBook.appendChild(author)
+        newBook.appendChild(pages)
+        newBook.appendChild(read)
+    })
+}
+
+const book1 = new Book("L'Etranger", "Albert Camus", 100, true, crypto.randomUUID())
+const book2 = new Book("Crime and punishment", "Dostoevsky", 300, true, crypto.randomUUID())
+const book3 = new Book("Brothers Karamazov", "Dostoevsky", 900, true, crypto.randomUUID())
+const book4 = new Book("The trial", "Kafka", 200, true, crypto.randomUUID())
+const book5 = new Book("Les Miserables", "Victor Hugo", 900, false, crypto.randomUUID())
+const book6 = new Book("Candide", "Voltaire", 300, false, crypto.randomUUID())
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+addBookToLibrary(book5)
+addBookToLibrary(book6)
+
+displayBooks()
